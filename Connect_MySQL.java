da740468cf367d07edd04b6e59bd7e602fe28b58
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-04-26 10:53:42 +0530

    Initial commit

---
+++ Connect_MySQL.java
@@ -0,0 +1,12 @@
+package jdbc_Connection;
+import java.sql.*;
+
+public class Connect_MySQL {
+
+	public static void main(String[] args)throws Exception {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+	}
+
+}
